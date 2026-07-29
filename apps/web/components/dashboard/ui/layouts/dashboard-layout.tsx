ed91fdba0548a83552ecd3b0b4ea907dd3ac0be8
Aman Kumar <aman.it360@gmail.com>
2026-07-29 21:57:19 +0530

    feat: add DashboardLayout component for improved dashboard structure

---
+++ apps/web/components/dashboard/ui/layouts/dashboard-layout.tsx
@@ -0,0 +1,23 @@
+import { AuthWrapper } from "@/components/auth/ui/components/AuthWrapper";
+import { OrganizationWrapper } from "@/components/auth/ui/components/OrganizationWrapper";
+import { SidebarProvider } from "@workspace/ui/components/sidebar";
+import { cookies } from "next/headers";
+import React from "react";
+import DashboardSidebar from "../components/dashboard-sidebar";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+  return (
+    <AuthWrapper>
+      <OrganizationWrapper>
+        <SidebarProvider defaultOpen={defaultOpen}>
+          <DashboardSidebar />
+          <main className="flex flex-1 flex-col">{children}</main>
+        </SidebarProvider>
+      </OrganizationWrapper>
+    </AuthWrapper>
+  );
+};
+
+export default DashboardLayout;
