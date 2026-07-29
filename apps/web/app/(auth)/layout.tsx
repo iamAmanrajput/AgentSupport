4cc680779e4aad83880627b894b77340e09e4f69
Aman Kumar <aman.it360@gmail.com>
2026-07-29 10:07:38 +0530

    feat: replace Layout component with AuthLayout for improved authentication UI

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
+import { AuthLayout } from "@/components/auth/ui/layouts/AuthLayout"
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex h-full min-h-screen min-w-screen flex-col items-center justify-center">
-      {children}
-    </div>
-  )
+  return <AuthLayout>{children}</AuthLayout>
 }
 
 export default Layout
