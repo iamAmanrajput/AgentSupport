13675e9ec4e342640b9131ddfe5af14ab865781b
Aman Kumar <aman.it360@gmail.com>
2026-08-04 10:06:38 +0530

    feat: add vapi setup files

--- apps/web/app/(dashboard)/plugins/vapi/page.tsx
+++ apps/web/app/(dashboard)/plugins/vapi/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+// import { PremiumFeatureOverlay } from "@/modules/billing/ui/components/premium-feature-overlay";
+import { VapiView } from "@/components/plugins/ui/views/vapi-view";
+// import { Protect } from "@clerk/nextjs";
 
-const Vapi = () => {
-  return <div>Vapi</div>;
+const Page = () => {
+  return (
+    // <Protect
+    //   condition={(has) => has({ plan: "pro" })}
+    //   fallback={
+    //     <PremiumFeatureOverlay>
+    //       <VapiView />
+    //     </PremiumFeatureOverlay>
+    //   }
+    // >
+    //   <VapiView />
+    // </Protect>
+    <VapiView />
+  );
 };
 
-export default Vapi;
+export default Page;
